78dde3e726fb259659dacde8021d304abdc91642
Andrei Cataraga <andreicataraga18@gmail.com>
2024-01-10 23:37:14 +0200

    Thread name and Thread priority

---
+++ src/multithreading/Ex5.java
@@ -0,0 +1,21 @@
+package multithreading;
+
+public class Ex5 {
+    public static void main(String[] args) {
+        MyThread5 myThread5 = new MyThread5();
+        myThread5.setName("Multimea mea");
+        myThread5.setPriority(Thread.MAX_PRIORITY);
+        System.out.println("Name of myThread5 = " + myThread5.getName()
+                + " Priority of myThread5 = " + myThread5.getPriority());
+        MyThread5 myThread6 = new MyThread5();
+        System.out.println("Name of myThread6 = " + myThread6.getName()
+                + " Priority of myThread6 = " + myThread6.getPriority());
+
+    }
+}
+
+class MyThread5 extends Thread {
+    public void run() {
+        System.out.println("Salut");
+    }
+}
