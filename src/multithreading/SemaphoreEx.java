e1be3fd1a1aec3f3384e55ad50f05545fdefff87
Andrei Cataraga <andreicataraga18@gmail.com>
2024-01-22 00:06:52 +0100

    New changes in SemaphoreEx

--- src/multithreading/SemaphoreEx.java
+++ src/multithreading/SemaphoreEx.java
@@ -5,30 +5,28 @@ import java.util.concurrent.Semaphore;
 public class SemaphoreEx {
     public static void main(String[] args) {
         Semaphore callBox = new Semaphore(2);
-
+new Person("Elena", callBox);
+new Person("Andrei", callBox);
+new Person("Tudor", callBox);
+new Person("Ana", callBox);
     }
 }
 
 class Person extends Thread {
     String name;
     private Semaphore callBox;
-
-    public Person(String name, Semaphore callBox);this.name =
-
-    name {
+    public Person(String name, Semaphore callBox) {
         this.name = name;
         this.callBox = callBox;
         this.start();
     }
-
     public void run() {
-        System.out.println(name + "asteapta");
         try {
+            System.out.println(name + " asteapta...");
             callBox.acquire();
-            System.out.println(name + "se foloseste de telefon");
-            sleep(20000);
-            System.out.println(name + "a terminat apelul");
-
+            System.out.println(name + " se foloseste de telefon");
+            sleep(2000);
+            System.out.println(name + " a terminat apelul");
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
