c97a431302635586f9c136f86a4fe0d7903a10bf
Andrei Cataraga <andreicataraga18@gmail.com>
2024-01-30 21:56:14 +0100

    DataInputStream and DataOutputStream

--- src/work_with_files/CopyEx.java
+++ src/work_with_files/CopyEx.java
@@ -5,19 +5,19 @@ import java.io.*;
 public class CopyEx {
     public static void main(String[] args) {
         try (BufferedReader reader = new BufferedReader
-                (new FileReader("C:\\Users\\ionla\\OneDrive\\Рабочий стол\\FC_Barcelona_(crest).svg.webp"));
+                (new FileReader("test2.txt"));
              BufferedWriter writer = new BufferedWriter(
-                     new FileWriter("FC_Barcelona.webp"));
+                     new FileWriter("test3.txt"));
         ) {
-            int character;
-            while ((character = reader.read()) != -1) {
-                writer.write(character);
-            }
-//            String line;
-//            while ((line = reader.readLine()) != null) {
-//                writer.write(line);
-//                writer.write('\n');
+//            int character;
+//            while ((character = reader.read()) != -1) {
+//                writer.write(character);
 //            }
+            String line;
+            while ((line = reader.readLine()) != null) {
+                writer.write(line);
+                writer.write('\n');
+            }
             System.out.println("Done!");
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
