17133ed37365a5785fba1d488e554fb366042a71
Andrei Cataraga <andreicataraga18@gmail.com>
2024-01-30 21:03:35 +0100

    Try with Resources

--- src/work_with_files/FileReaderEx.java
+++ src/work_with_files/FileReaderEx.java
@@ -1,24 +1,25 @@
 package work_with_files;
 
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 
 public class FileReaderEx {
     public static void main(String[] args) throws IOException {
-        FileReader reader = null;
-        try {
-            reader = new FileReader("test2.txt");
+//        FileReader reader = null;
+        try (FileReader reader = new FileReader("test2.txt");
+             FileWriter writer = new FileWriter("test2.txt", true); ) {
             int character;
-            while ((character = reader.read())!= -1){
-                System.out.print((char)character);
+            while ((character = reader.read()) != -1) {
+                System.out.print((char) character);
             }
             System.out.println();
             System.out.println("Done!");
         } catch (IOException e) {
             e.printStackTrace();
         }
-        finally {
-            reader.close();
-        }
+//        finally {
+//            reader.close();
+//        }
     }
 }
--- src/work_with_files/FileWriterEx.java
+++ src/work_with_files/FileWriterEx.java
@@ -8,21 +8,20 @@ public class FileWriterEx {
         String rubai = "Pentru ca raul sa triumfe este suficient ca oamenii buni sa nu faca nimic" +
                 " Edmund Burke";
         String s = " Salut!\n";
-        FileWriter writer = null;
-        try {
-            writer = new FileWriter("C:\\Users\\ionla\\OneDrive\\Рабочий стол\\text1");
-//            writer = new FileWriter("test2.txt", true);
-//            for(int i = 0; i<rubai.length(); i++) {
-//                writer.write(rubai.charAt(i));
-//            }
-            writer.write(rubai);
+
+        try (FileWriter writer = new FileWriter("test2.txt", true)) {
+//            writer = new FileWriter("C:\\Users\\ionla\\OneDrive\\Рабочий стол\\text1");
+            for (int i = 0; i < rubai.length(); i++) {
+                writer.write(rubai.charAt(i));
+            }
+//            writer.write(rubai);
             writer.write(s);
             System.out.println("Done!");
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            e.printStackTrace();
         }
-        finally {
-            writer.close();
-        }
+//        finally {
+//            writer.close();
+//        }
     }
 }
