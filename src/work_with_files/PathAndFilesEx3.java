25fbf0af9657686c5b5db25f646a69e2463c4af0
Andrei Cataraga <andreicataraga18@gmail.com>
2024-02-05 22:35:11 +0100

    Interface Path and class Files, Part3

---
+++ src/work_with_files/PathAndFilesEx3.java
@@ -0,0 +1,23 @@
+package work_with_files;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class PathAndFilesEx3 {
+    public static void main(String[] args) throws IOException {
+
+        Path filePath = Paths.get("test10.txt");
+//        Files.createFile(filePath);
+//        String dialog = "-Salut\n-Salut\n-Cum merg treburile?\n-Bine, la tine cum?\n-Tot bine!";
+//        Files.write(filePath, dialog.getBytes());
+
+        List<String> list = Files.readAllLines(filePath);
+        for (String s: list){
+            System.out.println(s);
+        }
+
+    }
+}
